abb6425aed38938bef924d49cd29b9b5dd14c9b8
mallikapps <mallik.techno@gmail.com>
2022-05-17 15:32:34 +0530

    Created User Model class with three properties

---
+++ MyTouristGuide/User.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MyTouristGuide
+{
+    public class User
+    {
+        public int UserId { set; get; }
+        public string UserName { set; get; }
+        public string Password { set; get; }
+    }
+}
