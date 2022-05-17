7821ea534ef4452d2df515b604ec9d72625210d8
mallikapps <mallik.techno@gmail.com>
2022-05-17 16:07:21 +0530

    to maintain login and logut time of each user

---
+++ MyTouristGuide/UserLog.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MyTouristGuide
+{
+    public class UserLog
+    {
+        public int UserId { set; get; }
+        public DateTime InTime { set; get; }
+        public DateTime OutTime { set; get; }
+    }
+}
